afc4a966f17eab9cb4925dd19fdd8437cfb80fb3
Mannan Ansari <mannanansari3in1@gmail.com>
2024-11-28 12:12:32 +0500

    Random Commit.

--- src/com/project/backend/Shift.java
+++ src/com/project/backend/Shift.java
@@ -2,6 +2,6 @@ package com.project.backend;
 
 public class Shift{
     public static void main(String[] args){
-        System.out.print("Hello Nigga!");
+        System.out.print("Hello there!");
     }
 }
